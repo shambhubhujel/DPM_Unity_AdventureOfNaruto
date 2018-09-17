dd548ba352c762e003ace5cb19ac167760f2a493
Abiralrai <abiralrai182@gmail.com>
2018-09-17 13:28:43 +1000

    Added Audio| Quit and Menu Button

---
+++ Assets/onQuit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quitGame : MonoBehaviour {
+
+ 
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
